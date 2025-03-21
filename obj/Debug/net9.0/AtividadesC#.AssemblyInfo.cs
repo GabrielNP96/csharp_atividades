43c600c9d88a84b548e030f454a5feeff492f734
GabrielNP96 <gabrielnp1996@gmail.com>
2025-03-21 10:10:38 -0300

    add .gitingore

--- obj/Debug/net9.0/AtividadesC#.AssemblyInfo.cs
+++ obj/Debug/net9.0/AtividadesC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtividadesC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9918599cc2f0b95a1f710e2ecb9ec5997ed658f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtividadesC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtividadesC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
