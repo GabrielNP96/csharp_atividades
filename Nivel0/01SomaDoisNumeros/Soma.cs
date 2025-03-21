43b1ff94cd8a09f70f28952319fd62c680b57013
GabrielNP96 <gabrielnp1996@gmail.com>
2025-03-21 11:05:28 -0300

    problema do readline resolvido

--- Nivel0/01SomaDoisNumeros/Soma.cs
+++ Nivel0/01SomaDoisNumeros/Soma.cs
@@ -4,9 +4,9 @@ class Soma
 {
     static void Main() {
         Console.WriteLine("Vamos somar dois Números!\nDigite um número: ");
-        int number1 = int.Parse(Console.ReadLine());
+        int number1 = int.Parse(Console.ReadLine()!);
         Console.WriteLine("Digite outro número: ");
-        int number2 = int.Parse(Console.ReadLine());
+        int number2 = int.Parse(Console.ReadLine()!);
         int result = number1 + number2;
         Console.WriteLine("A soma é " +  result);
     }
