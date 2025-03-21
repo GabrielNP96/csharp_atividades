0248ad2375e7585380bb3cb6832ed61342d45c5a
GabrielNP96 <gabrielnp1996@gmail.com>
2025-03-21 11:12:08 -0300

    contagem regressiva

---
+++ Nivel0/03Countdown/Program.cs
@@ -0,0 +1,13 @@
+﻿//Solicite ao usuário um número e faça uma contagem regressiva até 0.
+class Countdown
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um número inteiro positivo para realizar a contagem regressiva: ");
+        int userNumber = int.Parse(Console.ReadLine()!);
+        for(int i = userNumber; i >= 0; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
--- obj/Debug/net9.0/AtividadesC#.AssemblyInfo.cs
+++ obj/Debug/net9.0/AtividadesC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtividadesC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43c600c9d88a84b548e030f454a5feeff492f734")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43b1ff94cd8a09f70f28952319fd62c680b57013")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtividadesC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtividadesC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
