62fd8c23874f834d1ca3e5207c8278d80df49dd1
GabrielNP96 <gabrielnp1996@gmail.com>
2025-03-21 11:03:40 -0300

    Nova atividade: par ou ímpar

---
+++ Nivel0/02ParOuImpar/Program.cs
@@ -0,0 +1,22 @@
+﻿//Peça ao usuário para inserir um número e diga se ele é par ou ímpar.
+
+class EvenOrOdd
+{
+    static void Main() {
+        string EvenOrOdd()
+        {
+         Console.Write("Vamos descobrir se um número é par ou impar.\nDigite um número inteiro: ");
+         int number = int.Parse(Console.ReadLine()!);
+        
+            if(number % 2 == 0)
+            {
+                return $"{number} é par";
+            } else
+            {
+            return $"{number} é ímpar";
+            }
+        }
+        Console.WriteLine(EvenOrOdd());
+
+    }
+}
