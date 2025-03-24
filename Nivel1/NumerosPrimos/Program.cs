ffa23750327638926eeb4702bed4c07b04adab0d
GabrielNP96 <gabrielnp1996@gmail.com>
2025-03-24 16:15:15 -0300

    Números primos

---
+++ Nivel1/NumerosPrimos/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+class PrimeNumbers
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite um número inteiro positivo: ");
+        int number = int.Parse(Console.ReadLine()!);
+        bool isPrime = true;
+
+        if(number < 2)
+        {
+            Console.WriteLine($"{number} não é primo.");
+        }else {
+            for(int i = 2; i <= Math.Sqrt(number); i++ )
+            {
+                if(number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                } 
+            }
+        }
+
+        if(isPrime)
+        {
+            Console.WriteLine($"{number} é primo");
+        } else
+        {
+            Console.WriteLine($"{number} não é primo.");
+        }
+    }
+}
--- obj/Debug/net9.0/AtividadesC#.AssemblyInfo.cs
+++ obj/Debug/net9.0/AtividadesC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtividadesC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0248ad2375e7585380bb3cb6832ed61342d45c5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5afdfa9479d4b462db718f45ede72dc1257a4d0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtividadesC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtividadesC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
